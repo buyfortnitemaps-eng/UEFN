a07425069d5237aefd6fe49f9d39eb84a83bb016
mdAbirDeveloper <mdabir.developer@gmail.com>
2026-02-26 12:57:38 +0600

    after fix some color

--- src/app/shared/navber/page.js
+++ src/app/shared/navber/page.js
@@ -119,7 +119,7 @@ const Navbar = () => {
               type="text"
               value={searchQuery}
               onChange={(e) => setSearchQuery(e.target.value)}
-              placeholder="Search assets..."   className="w-full h-full bg-transparent pl-10 pr-4 text-sm text-white placeholder:text-white/20 outline-none transition-all"
+              placeholder="Search assets..."   className="w-full h-full bg-transparent pl-10 pr-4 text-sm text-white placeholder:text-forground outline-none transition-all border border-border-color rounded-2xl"
             />
           </div>
 
